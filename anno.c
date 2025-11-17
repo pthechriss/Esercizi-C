7a1fb8ed19605b9847f46c5831c6215988e85cbb
pthechriss <palaghianucristian11@gmail.com>
2025-11-17 23:59:32 +0100

    aggiorno

--- anno.c
+++ anno.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int m, a;
     printf("Insert month and year:\n");
     scanf("%d%d", &m, &a);
-    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
-    printf("31\n");
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+    {
+        printf("31\n");
     }
-    else 
-    if(m == 4 || m == 6 || m == 9 || m == 11) {
-    printf("30\n");
-}
-    if(m ==2)
-    if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
-    printf("29\n");
-    else
-    printf("28\n");
+    else if (m == 4 || m == 6 || m == 9 || m == 11)
+    {
+        printf("30\n");
+    }
+    if (m == 2)
+        if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
+            printf("29\n");
+        else
+            printf("28\n");
 }
