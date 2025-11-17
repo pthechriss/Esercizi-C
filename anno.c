e8a6b0ed3608733613e4927e6ca4466eec340626
pthechriss <palaghianucristian11@gmail.com>
2025-11-17 23:45:48 +0100

    Aggiornamento

--- anno.c
+++ anno.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int anno;
-    printf("Insert year:\n");
-    scanf("%d", &anno);
-    if ((anno % 4 == 0 && anno % 100 != 0) || anno % 400 == 0)
-    printf("Bisestile\n");
+    int m, a;
+    printf("Insert month and year:\n");
+    scanf("%d%d", &m, &a);
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+    printf("31\n");
+    }
+    else 
+    if(m == 4 || m == 6 || m == 9 || m == 11) {
+    printf("30\n");
+}
+    if(m ==2)
+    if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
+    printf("29\n");
     else
-    printf("Non bisestile\n");
+    printf("28\n");
 }
